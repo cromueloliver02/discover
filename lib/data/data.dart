14e3b673eb43d33bc7e37ce10aa085516f697dc7
cromueloliver02 <cromuelbarut@gmail.com>
2022-10-27 00:53:03 +0800

    added small missing details

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -24,39 +24,42 @@ class Article {
 
 List<Article> articles = [
   Article(
-    title: 'Japan\'s second largest metropolitan area',
+    title:
+        'Osaka is a major financial center of Japan, and it is recognized as one of the most multicultural and cosmopolitan cities in Japan.',
     image:
         'https://www.planetware.com/photos-large/JPN/japan-attractions-hiroshima-itsukushima.jpg',
     comments: 100,
     likes: 32000,
     isLiked: true,
-    location: 'Osaka Japan',
+    location: 'Osaka, Japan',
     shares: 50,
     author: 'Kentarō Hiroki',
     authorImage:
         'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
   ),
   Article(
-    title: 'Known as the sleepless town for obvious reasons',
+    title:
+        'Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples.',
     image:
         'https://worldtourtips.com/wp-content/uploads/2020/11/Japan-1-e1606077075928.jpg',
     comments: 300,
     likes: 50000,
-    isLiked: true,
-    location: 'Kabuikicho Japan',
+    isLiked: false,
+    location: 'Tokyo, Japan',
     shares: 1250,
     author: 'Minako Kanna',
     authorImage:
         'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
   ),
   Article(
-    title: 'Japan\'s second largest metropolitan area',
+    title:
+        'Once the capital of Japan, is a city on the island of Honshu. It\'s famous for its numerous classical Buddhist temples, as well as gardens, imperial palaces.',
     image:
         'https://blog.japanwondertravel.com/wp-content/uploads/2018/12/sorasak-_UIN-pFfJ7c-unsplash.jpg',
     comments: 200,
     likes: 10000,
     isLiked: true,
-    location: 'Tokyo Japan',
+    location: 'Kyoto, Japan',
     shares: 1000,
     author: 'Kumiko Yumiko',
     authorImage:
--- lib/widgets/dcv_place_card.dart
+++ lib/widgets/dcv_place_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import '../utils/constants.dart';
 import '../data/data.dart';
 
@@ -13,33 +14,49 @@ class DCVArticleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(50),
-      child: SizedBox(
-        height: 250,
-        child: Stack(
-          children: [
-            CachedNetworkImage(
+    return SizedBox(
+      height: 280,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: CachedNetworkImage(
               imageUrl: article.image,
+              height: 250,
               width: double.infinity,
               fit: BoxFit.cover,
             ),
-            Container(color: Colors.black26),
-            _DCVArticleCardHeader(
-              author: article.author,
-              authorImage: article.authorImage,
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.black26,
+              borderRadius: BorderRadius.circular(50),
             ),
-            Align(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 40),
-                child: _DCVArticleCardBody(
-                  title: article.title,
-                  location: article.location,
-                ),
+          ),
+          _DCVArticleCardHeader(
+            author: article.author,
+            authorImage: article.authorImage,
+          ),
+          Align(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 40),
+              child: _DCVArticleCardBody(
+                title: article.title,
+                location: article.location,
               ),
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: _DCVArticleCardFooter(
+              likes: article.likes,
+              comments: article.comments,
+              shares: article.shares,
+              isLiked: article.isLiked,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -132,57 +149,155 @@ class _DCVArticleCardBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle,
+    return Padding(
+      padding: const EdgeInsets.only(right: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(
+              Icons.play_arrow,
+              color: kPrimaryColor,
+              size: 30,
+            ),
           ),
-          child: const Icon(
-            Icons.play_arrow,
-            color: kPrimaryColor,
-            size: 30,
-          ),
-        ),
-        const SizedBox(width: 10),
-        Flexible(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Flexible(
-                child: Text(
-                  title,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
+          const SizedBox(width: 10),
+          Flexible(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Text(
+                    title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Flexible(
-                child: Text(
-                  location,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: Colors.grey[200],
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
+                const SizedBox(height: 10),
+                Flexible(
+                  child: Text(
+                    location,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.grey[200],
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
+    );
+  }
+}
+
+class _DCVArticleCardFooter extends StatelessWidget {
+  final int likes;
+  final int comments;
+  final int shares;
+  final bool isLiked;
+
+  const _DCVArticleCardFooter({
+    Key? key,
+    required this.likes,
+    required this.comments,
+    required this.shares,
+    required this.isLiked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 250,
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 2),
+            blurRadius: 6,
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _DCVArticleCardActionButton(
+            iconData: isLiked ? Icons.favorite : Icons.favorite_border,
+            color: Colors.red,
+            count: likes,
+            onTap: () {},
+          ),
+          _DCVArticleCardActionButton(
+            iconData: MdiIcons.chat,
+            count: comments,
+            onTap: () {},
+          ),
+          _DCVArticleCardActionButton(
+            iconData: Icons.share,
+            count: shares,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DCVArticleCardActionButton extends StatelessWidget {
+  final IconData iconData;
+  final int count;
+  final VoidCallback onTap;
+  final Color color;
+
+  const _DCVArticleCardActionButton({
+    Key? key,
+    required this.iconData,
+    required this.count,
+    required this.onTap,
+    // ignore: unused_element
+    this.color = Colors.black54,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            iconData,
+            color: color,
+            size: 25,
+          ),
+          const SizedBox(width: 2),
+          Text(
+            count.toString(),
+            style: TextStyle(color: color),
+          ),
+        ],
+      ),
     );
   }
 }
